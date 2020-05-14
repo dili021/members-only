225f958e0998c490582dc52601dfa1a17cf3f107
Stefan Dili <stefan.dili.021@gmail.com>
2020-05-14 10:07:59 +0200

    built relations between posts and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
+    @post = current_user.posts.build
   end
 
   def create
