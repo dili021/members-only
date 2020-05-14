701d3c7e690cd62c40964765d9ddd0c6522a54a5
Stefan Dili <stefan.dili.021@gmail.com>
2020-05-14 10:01:42 +0200

    added name column to users

---
+++ db/migrate/20200514080038_add_name_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameToUsers < ActiveRecord::Migration[6.0]
+  def change
+
+    add_column :users, :name, :string
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
